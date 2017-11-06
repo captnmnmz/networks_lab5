38950a325a95ca71791a18da28353ef2310384ac
bastienchevallier <32243212+bastienchevallier@users.noreply.github.com>
2017-11-06 23:21:01 +0100

    DebugReceiver proposal

---
+++ src/DebugReceiver.java
@@ -0,0 +1,35 @@
+
+public class DebugReceiver implements SimpleMessageHandler, Runnable {
+	private MuxDemuxSimple myMuxDemux= null;
+	private SynchronizedQueue incoming = new SynchronizedQueue(20);
+	
+	@Override
+	public void run() {
+		String s = incoming.dequeue();
+		try {
+			//Print raw message
+			System.out.println("Debug : " + s );
+			if (s.toLowerCase().contains("exception")) {
+				//TODO DO SOMETHING
+			}
+		}catch(Exception e) {
+			
+		}
+		
+	}
+
+	@Override
+	public void handleMessage(String m) {
+		try {
+			incoming.enqueue(m);
+		}catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	@Override
+	public void setMuxDemux(MuxDemuxSimple md) {
+		myMuxDemux = md;
+	}
+
+}
