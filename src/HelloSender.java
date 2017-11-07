0567c073c6bcba7907937a2daf4a80c5eda8466d
bastienchevallier <32243212+bastienchevallier@users.noreply.github.com>
2017-11-07 14:47:50 +0100

    HelloSender send repeatedly

--- src/HelloSender.java
+++ src/HelloSender.java
@@ -1,11 +1,12 @@
 import java.lang.Object;
 import java.util.Random;
-
+import java.util.Timer;
+import java.util.TimerTask;
 
 class RandomAlphanumeric {
 	private static String letters = "abcdefghijklmnopqrstuvwxyz";
 	private static char[] alphanumeric = (letters + letters.toUpperCase()+ "0123456789").toCharArray();
-	
+
 	public static String generateRandomAlphanumeric(int length) {
 		StringBuilder s = new StringBuilder();
 		for (int i=0; i<length; i++) {
@@ -17,21 +18,27 @@ class RandomAlphanumeric {
 
 
 public class HelloSender implements SimpleMessageHandler, Runnable {
-	
+
 	private MuxDemuxSimple myMuxDemux= null;
 	private SynchronizedQueue incoming = new SynchronizedQueue(20);
-	 
-	//TODO repeat each HelloInterval
+	private Timer TIMER = new Timer("SendingTimer", true);;
+
 	public void run() {
 		try {
 			String senderID = RandomAlphanumeric.generateRandomAlphanumeric(16);
 			int HelloInterval = new Random().nextInt(256);
 			HelloMessage m = new HelloMessage(senderID,1,HelloInterval);
-			myMuxDemux.send(m.getHelloMessageAsEncodedString());
+			TimerTask task = new TimerTask() {
+				@Override
+				public void run() {
+					myMuxDemux.send(m.getHelloMessageAsEncodedString());
+				}
+			}; 
+			TIMER.schedule(task, 0, HelloInterval);
+
 		}catch(IllegalArgumentException e) {
 			System.err.println(e.getMessage());
 		}
-		
 	}
 	@Override
 	public void handleMessage(String m) {
@@ -46,6 +53,6 @@ public class HelloSender implements SimpleMessageHandler, Runnable {
 	public void setMuxDemux(MuxDemuxSimple md) {
 		myMuxDemux = md;
 	}
-	
+
 
 }
