f9c6ca5688588d7c0f2fd24839da1101c140611d
Oliver Yates <oliver.yates01@gmail.com>
2017-11-07 17:41:08 +0100

    finished peertable

--- src/PeerTable.java
+++ src/PeerTable.java
@@ -8,17 +8,16 @@ import java.util.NoSuchElementException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.Instant;
 import java.util.Hashtable;
 
 class PeerRecord{
 	String peerID;
 	InetAddress peerIPAddress;
 	int peerSeqNum;
-	Instant expirationTime;
+	double expirationTime;
 	PeerState peerState;
 
-	public PeerRecord(String peerID,InetAddress peerIPAddress, int peerSeqNum, Instant expirationTime, PeerState peerState){
+	public PeerRecord(String peerID,InetAddress peerIPAddress, int peerSeqNum, double expirationTime, PeerState peerState){
 		this.peerID=peerID;
 		this.peerIPAddress=peerIPAddress;
 		this.peerSeqNum=peerSeqNum;
@@ -30,18 +29,31 @@ class PeerRecord{
 public class PeerTable {
 	Hashtable<String,PeerRecord> table;
 
-	public synchronized void put(PeerRecord peerRecord){
+	public synchronized void addPeer(PeerRecord peerRecord){
 		table.put(peerRecord.peerID, peerRecord);
 	}
 
-	public synchronized PeerRecord get(String peerID){
-		if (table.contains(peerID)){
-			return table.get(peerID);
+	public synchronized PeerRecord getPeer(String peerID){
+		if (table.containsKey(peerID)){
+			PeerRecord peer = table.get(peerID);
+			if (peer.expirationTime<System.currentTimeMillis()){
+				peer.peerState=PeerState.DYING;
+				table.remove(peerID);
+				return null;
+			}else{
+				return peer;
+			}
 		}
+		return null;
+
 	}
 
 	public synchronized boolean contains(PeerRecord peerRecord){
 		return table.containsKey(peerRecord.peerID);
+	}
+	
+	public synchronized String toString(){
+		return table.toString();
 	}
 
 
