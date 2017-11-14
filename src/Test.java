e1c33fcbbb72b6abdfb368d92c4b83e8ac889a42
bastienchevallier <32243212+bastienchevallier@users.noreply.github.com>
2017-11-14 22:46:27 +0100

    Program runs correctly

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 
 
 import java.net.DatagramSocket;
+import java.net.SocketException;
 
 import HelloMessage.DebugReceiver;
 import HelloMessage.HelloReceiver;
@@ -14,27 +15,31 @@ import ListMessage.ListReceiver;
 public class Test {
 
 	public static void main(String[] args) {
-		try {
-			DatagramSocket mySocket = new DatagramSocket(4242);
-			mySocket.setBroadcast(true);
-			SimpleMessageHandler[] handlers = new SimpleMessageHandler[3];
-			handlers[0]= new HelloReceiver();
-			handlers[1]= new HelloSender();
-			handlers[2]= new DebugReceiver();
-			handlers[3]= new SynReceiver();
-			handlers[4]= new ListReceiver();
-			handlers[5]= new SynSender();
-			MuxDemuxSimple dm = new MuxDemuxSimple(handlers, mySocket,"Bastien", 254);
-			new Thread(dm).start();
-			new Thread(handlers[0]).start();
-			new Thread(handlers[1]).start();
-			new Thread(handlers[2]).start();
-			new Thread(handlers[3]).start();
-			new Thread(handlers[4]).start();
-			new Thread(handlers[5]).start();
-	    }catch(Exception e ) {
-	    		System.err.println("Get error : " + e.getMessage());
-	    }
+			DatagramSocket mySocket;
+			try {
+				mySocket = new DatagramSocket(4242);
+				mySocket.setBroadcast(true);
+				SimpleMessageHandler[] handlers = new SimpleMessageHandler[6];
+				handlers[0]= new HelloReceiver();
+				handlers[1]= new HelloSender();
+				handlers[2]= new DebugReceiver();
+				handlers[3]= new SynReceiver();
+				handlers[4]= new ListReceiver();
+				handlers[5]= new SynSender();
+				MuxDemuxSimple dm = new MuxDemuxSimple(handlers, mySocket,"Bastien", 254);
+				new Thread(dm).start();
+				new Thread(handlers[0]).start();
+				new Thread(handlers[1]).start();
+				new Thread(handlers[2]).start();
+				new Thread(handlers[3]).start();
+				new Thread(handlers[4]).start();
+				new Thread(handlers[5]).start();
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+
 
 	}
 
